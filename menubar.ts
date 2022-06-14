b8bf4336d2814f6efce964e0ce02cac3541d82ea
AsrielPires <bebether@gmail.com>
2022-06-14 14:15:39 +0100

    general

--- menubar.ts
+++ menubar.ts
@@ -3,6 +3,7 @@ import { $, C, HAlign, hc, icon, Icon } from "./galhui";
 
 export type Items = any;//Array<One | Array<Items>>;
 export const menubar = (...items: Items) => div("_ bar", items);
-export const item = (i: Icon, text: any, action?: (e: MouseEvent) => any) => g("button", C.item, [icon(i), text]).on("click", action);
+export const item = (i: Icon, text: any, action?: (e: MouseEvent) => any) => g("button", "i", [icon(i), text]).on("click", action);
+export const a = (i: Icon, text: any,href?:str) => g("a", "i", [icon(i), text]).prop("href", href);
 export const right = () => div(HAlign.right);
 export const sep = () => g("hr");
