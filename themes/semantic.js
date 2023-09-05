42dbb05ce461463fceedf9fbcffd8837d67e9e5a
asriel carneiro <bebether@gmail.com>
2023-09-06 00:44:19 +0100

    general

--- themes/semantic.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2VtYW50aWMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJzZW1hbnRpYy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIn0=
