dff3f7c2f26ea891a37d879c8d72e7936af42e60
asriel carneiro <bebether@gmail.com>
2023-10-28 22:02:16 +0100

    update tasks

--- tasks.js
+++ tasks.js
@@ -1,15 +1,20 @@
 import { writeFile } from "fs/promises";
-import { } from "./themes/basic.js"
+import { style as basicStyle, darkTheme as basicDarkTheme, lightTheme as basicLightTheme } from "./themes/basic.js"
 import { } from "./themes/material.js"
 import { } from "./themes/semantic.js"
 import { } from "./themes/bootstrap.js"
 
 export async function buildThemes() {
-    throw "not implemented";
-    await Promise.all([
-        writeFile("themes/basic.css",),
-        writeFile("themes/material.css",),
-        writeFile("themes/semantic.css",),
-        writeFile("themes/bootstrap.css",),
-    ]);
+  function multColor(light, dark) {
+    let t = "@media(prefers-color-scheme";
+    return `${t}:light){${light.css}}${t}:dark){${dark.css}}`;
+  }
+  await Promise.all([
+    writeFile("themes/basic.css", multColor(basicStyle(basicLightTheme()),basicStyle(basicDarkTheme()))),
+    writeFile("themes/basic.dark.css", basicStyle(basicDarkTheme()).css),
+    writeFile("themes/basic.light.css", basicStyle(basicLightTheme()).css),
+    // writeFile("themes/material.css",),
+    // writeFile("themes/semantic.css",),
+    // writeFile("themes/bootstrap.css",),
+  ]);
 }
